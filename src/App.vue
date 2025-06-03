50fe1bdd882c31a3faa28b0b91b5825b1086ce6d
djdjz7 <djdjz7@icloud.com>
2025-06-03 15:24:15 +0800

    fix: bg on mobile safari

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,29 @@ import NewYearBg from './components/NewYearBg.vue'
 </script>
 
 <template>
+  <div class="bg"></div>
   <NewYearBg v-if="SiteConfiguration.theme === 'new-year'" />
   <Suspense>
     <MainComponent z-1 relative />
   </Suspense>
 </template>
+
+<style lang="css">
+.bg {
+  background-image: radial-gradient(circle at top left, rgb(255, 246, 218), #fff 30%);
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+
+@media (prefers-color-scheme: dark) {
+  .bg {
+    background-image: radial-gradient(circle at top left, rgb(52, 39, 1), #121212 30%);
+    background-color: #121212;
+  }
+}
+</style>
