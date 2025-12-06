9f6e4c6604f9a560a6a33d3898596494a32e9934
djdjz7 <djdjz7@icloud.com>
2025-12-06 12:51:50 +0800

    fix: correctly render math in excerpt
    by adding commonly used mathjax styles to global stylesheet

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@/assets/base.css'
 import '@/assets/codeblocks.css'
 import '@/assets/containers.css'
 import '@/assets/heimu.css'
+import '@/assets/math-common.css'
 
 import { createSSRApp, createApp as createSPAApp } from 'vue'
 import App from './App.vue'
