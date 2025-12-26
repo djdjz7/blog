b810b7f881337367cc86c554c8ec739a23cf384f
djdjz7 <djdjz7@icloud.com>
2025-12-26 22:19:43 +0800

    chore: bump deps & use vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'esbuild',
+    minify: 'oxc',
     cssMinify: true,
     rollupOptions: {
       external: ['/pagefind/pagefind.js'],
